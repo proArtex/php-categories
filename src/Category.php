9244b3f9f359899c764565353f5a9d9fef191cc3
proartex <proartex@mail.ru>
2016-07-17 03:34:48 +0300

    objects implementation

--- src/Category.php
+++ src/Category.php
@@ -16,4 +16,14 @@ class Category {
 
     public $children;
 
+    public function __construct($data) {
+        $allowedFields = ['id', 'parentId', 'level', 'name', 'slug', 'children'];
+
+        foreach($data as $key => $value) {
+            if (in_array($key, $allowedFields)) {
+                $this->$key = $value;
+            }
+        }
+    }
+
 }
--- src/Tree.php
+++ src/Tree.php
@@ -4,29 +4,33 @@ namespace PhpCategories;
 
 class Tree {
 
+    private $data;
+
     public function __construct($horizontalCategories) {
-        var_dump($this->build($horizontalCategories));
+        $this->build($horizontalCategories);
     }
 
-    private function build($horizontalCategories, $rootId = 0) {
-        $tree = [
-            'id'        => $rootId,
-            'slug'      => 'root',
-            'parent_id' => 0,
+    private function build($horizontalCategories) {
+        $tree = new Category([
+            'id'        => 0,
+            'parentId'  => 0,
             'level'     => 0,
+            'name'      => 'Root',
+            'slug'      => 'root',
             'children'  => []
-        ];
+        ]);
 
+        //TODO: compare prev and curr iteration counters instead
         $maxIterations = count($horizontalCategories);
         $iterations = 0;
 
         while (++$iterations < $maxIterations) {
             foreach ($horizontalCategories as $key => &$category) {
-                $parentCategory = & $this->findParentNode($tree, $category['parent_id']);
+                $parentCategory = & $this->findParentNode($tree, $category['parentId']);
 
                 if ($parentCategory) {
-                    $category['level'] = $parentCategory['level'] + 1;
-                    $parentCategory['children'][] = $category;
+                    $category['level'] = $parentCategory->level + 1;
+                    $parentCategory->children[] = new Category($category);
                     unset($horizontalCategories[ $key ]);
                 }
             }
@@ -37,18 +41,18 @@ class Tree {
             throw new \Exception('Max cycle of category-tree building has been reached');
         }
 
-        return $tree;
+        $this->data = $tree;
     }
 
     private function & findParentNode(&$node, &$parentId) {
-        if ($node['id'] == $parentId) {
+        if ($node->id == $parentId) {
             return $node;
         }
 
         $result = false;
 
-        if (!empty($node['children'])) {
-            foreach ($node['children'] as &$childNode) {
+        if (!empty($node->children)) {
+            foreach ($node->children as &$childNode) {
                 $result = & self::findParentNode($childNode, $parentId);
 
                 if ($result) {
